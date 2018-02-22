f72b4596d6841e15347887516e8817fbc518c5c9
Skyice <vespade@gmail.com>
2018-02-22 23:01:09 +0100

    Ajout de Sang Royale V dans les données

--- app_client/crownchest/crownchest.controller.js
+++ app_client/crownchest/crownchest.controller.js
@@ -16,7 +16,8 @@
     {"name":"Sang Royale", "id":"CJQLP2"}, 
     {"name":"Sang Royale II", "id":"2LUU0R0L"}, 
     {"name":"Sang Royale III", "id":"8CPG2YU"}, 
-    {"name":"Sang Royale IV", "id":"8GQL980P"}]
+    {"name":"Sang Royale IV", "id":"8GQL980P"},
+    {"name":"Sang Royale V", "id":"8VVGJPCR"}]
     vm.topchest = []
     
     for(var i=0; i < vm.sangRoyaleFamily.length; i++){
--- app_client/export/export.controller.js
+++ app_client/export/export.controller.js
@@ -14,7 +14,8 @@
     {"name":"Sang Royale", "id":"CJQLP2"}, 
     {"name":"Sang Royale II", "id":"2LUU0R0L"}, 
     {"name":"Sang Royale III", "id":"8CPG2YU"}, 
-    {"name":"Sang Royale IV", "id":"8GQL980P"}]
+    {"name":"Sang Royale IV", "id":"8GQL980P"}, 
+    {"name":"Sang Royale V", "id":"8VVGJPCR"}]
 
     vm.downloadActivity = function(){
       if(vm.selectedClan["id"]){
