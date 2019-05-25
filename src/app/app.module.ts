f30af0b0890337d99549ed558aaa756dc74eef7c
Illonyx <vespade@gmail.com>
2019-05-26 00:40:10 +0200

    Mise à niveau de Angular, résolution d'un bug important, installation de Angular Materials
    Début des travaux sur la partie export de l'IHM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
 import { HttpClientModule }    from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 import { AppComponent } from './app.component';
 import { ExportComponent } from './export/export.component'
@@ -12,8 +21,12 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 @NgModule({
 	imports: [
     BrowserModule, 
+    BrowserAnimationsModule, 
+    FormsModule, 
     AppRoutingModule, 
-    HttpClientModule
+    HttpClientModule, 
+    MatCardModule, MatSelectModule, MatFormFieldModule, MatButtonModule, MatProgressBarModule
+
   ],
   declarations: [
     AppComponent, 
--- src/app/export/export.component.ts
+++ src/app/export/export.component.ts
@@ -1,28 +1,40 @@
 import { Component, OnInit} from '@angular/core';
 import { ExportService } from './export.service';
+
+export interface Clan {
+  id : string;
+  name : string;
+}
+
 @Component({
   selector: 'export-root',
   templateUrl: './export.component.html',
   styleUrls: ['./export.component.css']
 })
-export class ExportComponent implements OnInit {
+export class ExportComponent {
   fileCheck=[]
+  selectedClan : Clan;
+  selectedType : string;
 
-  sangRoyaleFamily = [
+  clans : Clan[] = [
     {"name":"Sang Royale", "id":"CJQLP2"}, 
     {"name":"Sang Royale II", "id":"2LUU0R0L"}, 
     {"name":"Sang Royale III", "id":"8CPG2YU"}, 
     {"name":"Sang Royale IV", "id":"8GQL980P"}, 
-    {"name":"Sang Royale V", "id":"8VVGJPCR"}]
+    {"name":"Sang Royale V", "id":"8VVGJPCR"}];
+    
+  types : string[] = ["GDC", "Activite"];
 
   constructor(private exportService: ExportService){}
 
-   ngOnInit() {
+   /*ngOnInit() {
     this.exportService.getFileCheck().subscribe(data => {
       console.log(JSON.stringify(data));
       this.fileCheck=data;
     });
-  }
+  }*/
+
+
 
   
 }
